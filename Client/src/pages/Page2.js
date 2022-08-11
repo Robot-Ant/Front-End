e84d1798b494594d1ec686c78dbd5f0cbf806231
HongHyunKi <hk7286@naver.com>
2022-08-11 11:47:13 +0900

    update FinanceChart

--- Client/src/pages/Page2.js
+++ Client/src/pages/Page2.js
@@ -4,47 +4,61 @@ import { useEffect, useState } from "react";
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 import axios from 'axios';
+import Button from '@mui/material/Button';
+import FinanceLineChart from '../components/FinanceLineChart';
 
 function Page2() {
-    const [value, setValue] = useState('');
-    const [inputValue, setInputValue] = useState('');
-    const [financeList, setFinanceList] = useState([]);
+  const [value, setValue] = useState('');
+  const [financeList, setFinanceList] = useState([]);
+  const [financeData, setFinanceData] = useState({});
 
-    useEffect(() => {
-      axios.get('http://localhost:5000/finance/list')
-        .then(res => setFinanceList(res.data))
-        .catch(function (error) {
-          console.log(error);
-        })
-    }, [])
-    
-    return (
-        <Container>
-            <AutocompleteBox>
-                <div>{`value: ${value !== null ? `'${value}'` : 'null'}`}</div>
-                <div>{`inputValue: '${inputValue}'`}</div>
-                <br />
-                <Autocomplete
-                    value={value}
-                    onChange={(event, newValue) => {
-                        setValue(newValue);
-                    }}
-                    inputValue={inputValue}
-                    onInputChange={(event, newInputValue) => {
-                        setInputValue(newInputValue);
-                    }}
-                    id="controllable-states-demo"
-                    options= {financeList}
-                    sx={{ width: 300 }}
-                    renderInput={(params) => <TextField {...params} label="주식 검색" />}
-                />
-            </AutocompleteBox>
+  useEffect(() => {
+    axios.get('http://localhost:5000/finance/list')
+      .then(res => setFinanceList(res.data))
+      .catch(function (error) {
+        console.log(error);
+      })
+  }, [])
 
-            <TradeContainer>
-                <p>선택한 주식을 파라미터로 다시 서버에 요청 > 영업이익 주가 차트</p>
-            </TradeContainer>
-        </Container>
-    );
+  function getFinanceData(){
+    console.log(value)
+    if(value){
+      axios.get('http://localhost:5000/finance/data', {
+      params: {
+        id: value
+      }
+      })
+      .then(res => setFinanceData(res.data))
+      .catch(function (error) {
+        console.log(error);
+      })
+    }
+  }
+
+  return (
+    <Container>
+      <AutocompleteBox>
+        <br />
+        <Autocomplete style={{backgroundColor:'white', borderRadius:3}}
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+          id="controllable-states-demo"
+          options= {['카카오']}
+          sx={{ width: 300 }}
+          renderInput={(params) => <TextField {...params} label="주식 검색" />}
+        />
+        <Button style={{marginLeft:'1.5%'}}variant="contained" onClick={getFinanceData}>검색</Button>
+      </AutocompleteBox>
+
+      <TradeContainer>
+        <ChartBox>
+          <FinanceLineChart items = {financeData}></FinanceLineChart>
+        </ChartBox>
+      </TradeContainer>
+    </Container>
+  );
 }
 
 export default Page2;
@@ -86,14 +100,19 @@ const TradeContainer = styled.div`
   border-radius: 8px;
 `
 
-const FontBox = styled.div`
-  font-size: 1.3vw;
+const ChartBox = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
 
   @media only screen and (max-width: 720px) {
-    font-size: 1em;
+  overflow: scroll;
   }
 
-  @media only screen and (min-width: 721px) {
-    font-size: 1.5em;
+  @media only screen and (min-width: 1200px) {
+  width: 66%;
   }
 `
