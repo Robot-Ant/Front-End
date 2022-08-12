2693c7c824e2d4c2bb4bc6b52df92a9485c3c167
HongHyunKi <hk7286@naver.com>
2022-08-12 18:06:31 +0900

    Update 경고무시

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,5 +1,5 @@
+/* eslint-disable */
 import * as React from 'react';
-import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import ResponsiveAppBar from './components/AppBar';
 import BasicTabs from './pages/Tabs';
