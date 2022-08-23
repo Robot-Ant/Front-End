87a6d9fd516a0f67e5dd9a9a943d766b94449888
HongHyunKi <hk7286@naver.com>
2022-08-23 19:56:13 +0900

    뷰 크기 개선

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,18 +1,24 @@
 /* eslint-disable */
 import * as React from 'react';
-import Container from '@mui/material/Container';
+//import Container from '@mui/material/Container';
 import ResponsiveAppBar from './components/AppBar';
 import BasicTabs from './pages/Tabs';
+import styled from 'styled-components';
 
 function App() {
   return (
-    <Container fixed>
+    <Container>
       <ResponsiveAppBar></ResponsiveAppBar>
       <BasicTabs></BasicTabs>
-
     </Container>
   );
 }
 
 export default App;
 
+const Container = styled.div`
+  width: auto;
+  height: auto;
+  max-width: 1280px;
+  margin:auto;
+`
