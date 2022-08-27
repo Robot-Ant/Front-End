60613d2eb7f1410b028cf97119ad48e4293f3d78
HongHyunKi <hk7286@naver.com>
2022-08-28 06:03:02 +0900

    모바일 링크 추카

--- Client/src/components/AppBar.js
+++ Client/src/components/AppBar.js
@@ -85,7 +85,7 @@ const ResponsiveAppBar = () => {
             variant="h5"
             noWrap
             component="a"
-            href=""
+            href="/"
             sx={{
               mr: 2,
               display: { xs: 'flex', md: 'none' },
